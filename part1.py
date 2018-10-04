998f32a6bab4006d23a954f8cd8d8fdc4fa48d63
Ran Bi <35747244+iamranbi@users.noreply.github.com>
2018-10-04 04:19:46 -0400

    Update part1.py

--- part1.py
+++ part1.py
@@ -5,13 +5,16 @@ import re
 WORD_RE = re.compile(r"[\w']+")
 
 class MRMostUsedWord(MRJob):
+    #mapper that splits a line of input into words
     def mapper(self, _, line):
         for word in WORD_RE.findall(line):
             yield word.lower(), 1
-
+            
+    #combiner that sums up the count for each word
     def combiner(self, word, counts):
         yield word, sum(counts)
 
+     #reducer that sums up the count for each word 
     def reducer(self, word, counts):
         yield word, sum(counts)
 
